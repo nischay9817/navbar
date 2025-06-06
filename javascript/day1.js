435c8386f631836032dda9ce1b721d009ed8d587
nischayshrestha <nikechrestha@gmail.com>
2025-06-06 22:29:32 +0545

    array and object

---
+++ javascript/day1.js
@@ -0,0 +1,38 @@
+// var name1="Nischay Shrestha";
+// var name2="Ram Adikhari";
+// var name3="Shyam Rai";
+// console.log(name1);
+// console.log(name2);
+// console.log(name3);
+
+
+//array:
+var name=['Nischay Shrestha','Ram Adikhari','Shyam Rai'];
+// console.log(name[0])
+name.push("Hari lal Shrestha")
+console.log(name)
+name.pop()
+console.log(name);
+name.shift()
+console.log(name)
+name.unshift("Hari Lal Shrestha")
+console.log(name) 
+//object
+// var person1={
+//     name: "Nischay Shrestha",
+//     age: 21,
+//     address: "Dharan"
+// }
+// console.log(person1.name)
+// console.log(person1.age)
+// console.log(person1.address)
+// console.log(person1['name']);
+// console.log(person1['age']);
+// console.log(person1['address']) 
+// console.log(person1.name,person1.age,person1.address)
+//changing the value of key inside the object
+// person1.name="Ram Shrestha";
+// person1.age=22
+//deleting the address
+// delete person1.address
+// console.log(person1)
